16308eea4231f8687dc8e89db2516c4231dfd4a5
daariff <aariff.deen@gmail.com>
2018-03-11 15:18:04 -0700

    updated getCustomerInfo.php

--- customerBill/getCustomerInfo.php
+++ customerBill/getCustomerInfo.php
@@ -14,9 +14,90 @@ getCustInfo($arg1);
 
 function getCustInfo($number)
 {
-	$arr = array ( 0 => "Deen Aariff", 1 => "408-249-5666", 2 => "S00023", 3 => "Samsung", 4 => date("Y/m/d"));
-	
+	$conn = oci_connect('mcai','magstar816','dbserver.engr.scu/db11g');
+	if(!$conn) {
+		echo "1, Error Connecting To Database";
+		exit();
+	}
 
+	/* query for a customer name given the customer phone number */
+
+	$queryString = "SELECT name FROM Customers WHERE phoneNo = :phone";
+
+	$query = oci_parse($conn,$queryString);
+	oci_bind_by_name($query,':phone',$number);
+	$res = oci_execute($query);
+
+	if(!$res) {
+		echo "1, Error in Database Query";
+		exit();
+	}
+
+	if(($row=oci_fetch_array($query,OCI_BOTH)) != false) {
+		$name = $row[0];
+	} else {
+		echo "1, Invalid Phone Number Provided";
+		exit();
+	}
+
+	/* query for all the information in the customer bill */
+	
+	$queryString = "SELECT itemId, contractId, description, timeOfArrival FROM RepairLog WHERE custPhone = :phone";
+
+	$query = oci_parse($conn,$queryString);
+	oci_bind_by_name($query,':phone',$number);
+	$res = oci_execute($query);
+
+	if(!$res) {
+		echo "1, Error in Database Query";
+		exit();
+	}
+
+	/* creates arrays for storing data before serialization */
+	$models = array();
+	$contracts = array();
+	$dates = array();
+	$descriptions = array();
+
+	/* iterate over all repsonses that match */
+
+	while(($row=oci_fetch_array($query,OCI_BOTH)) != false) {
+
+		// do a nother query here to get the model for a given item id
+
+		$queryString2 = "SELECT model FROM RepairItem WHERE itemId = :id";
+
+		$query2 = oci_parse($conn,$queryString);
+		/* bind to the item id */
+		oci_bind_by_name($query,':itemId',$row[0]);
+		$res = oci_execute($query);
+
+		if(!$res) {
+			echo "1, Error in Database Query";
+			exit();
+		}
+
+		/* push the model in to the model array */
+
+		if(($row2=oci_fetch_array($query,OCI_BOTH)) != false) {
+			array_push($models,$row2[0]);
+		}
+
+		/* push all other data into respective arrays */
+
+		array_push($contracts,$row[1]);
+		array_push($descriptions,$row[2]);
+		array_push($dates,$row[3]);
+	}
+
+	/* serialize the data for consumption by the front-end */
+
+	$contract_str = implode("|",$contracts);
+	$model_str = implode("|",$models);
+	$description_str = implode("|",$descriptions);
+	$dates_str = implode("|",$dates);
+
+	$arr = array ( 0 => $name, 1 => $number, 2 => $contract_str, 3 => $model_str, 4 => $description_str, 5 => $contract_date);
 	$str = implode (",", $arr);
     echo "0," . $str;
 }
