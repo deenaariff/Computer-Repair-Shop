8d0a30aca19a856bfe53f64778438a4865a8c4f7
maggie lingqi cai <mcai@linux60814.dc.engr.scu.edu>
2018-03-11 18:01:15 -0700

    minor update

--- sql/procedures.sql
+++ sql/procedures.sql
@@ -5,17 +5,20 @@
 Create or Replace Procedure newCustomer(n_name in VARCHAR, n_phone in VARCHAR)
 AS
 BEGIN
-	Insert into Customer values(n_name, n_phone);
+	Insert into Customers values(n_phone, n_name);
+	commit;
 END;
 /
+Show errors;
 
-Create or Replace Procedure acceptMachine(n_name in VARCHAR, model in VARCHAR, cId in VARCHAR, in_date in DATE)
+Create or Replace Procedure acceptMachine(n_name in VARCHAR, n_item in VARCHAR, model in VARCHAR, cId in VARCHAR, in_date in DATE)
 AS
 
 l_contract ServiceContract%rowtype;
 l_phone Customers.phoneNo%type;
 l_price RepairItem.price%type;
 l_year RepairItem.year%type;
+l_date DATE := to_date(in_date, 'DD-MM-YY');
 
 BEGIN
 
@@ -27,8 +30,8 @@ BEGIN
 	From Customers
 	Where name = n_name;
 
-	IF to_date(in_date,'DD-MM-YYYY') >= l_contract.startDate
-		AND to_date(in_date, 'DD-MM-YYYY') < l_contract.endDate THEN
+	IF l_date >= l_contract.startDate
+		AND l_date < l_contract.endDate THEN
 
 		IF l_contract.itemId1 IS NULL THEN
 			Update ServiceContract
@@ -36,7 +39,7 @@ BEGIN
 			Where cId = l_contract.contractId;
 
 			Insert into RepairItem values(n_item, model, l_price, l_year, l_contract.contractType, 'COMPUTER');
-			Insert into RepairJob values(n_item, l_contract.contractId, l_phone, NULL, to_date(SYSDATE, 'DD-MM-YYYY'), 'UNDER_REPAIR');
+			Insert into RepairJob values(n_item, l_contract.contractId, l_phone, NULL, to_date(SYSDATE, 'DD-MM-YY'), 'UNDER_REPAIR');
 		ELSE
 			IF l_contract.contractType = 'GROUP' THEN
 				Update ServiceContract
@@ -44,19 +47,21 @@ BEGIN
 				Where cId = l_contract.contractId;
 				
 				Insert into RepairItem values(n_item, model, l_price, l_year, l_contract.contractType, 'PRINTER');
-				Insert into RepairJob values(n_item, l_contract.contractId, l_phone, NULL, to_date(SYSDATE, 'DD-MM-YYYY'), 'UNDER_REPAIR');
+				Insert into RepairJob values(n_item, l_contract.contractId, l_phone, NULL, to_date(SYSDATE, 'DD-MM-YY'), 'UNDER_REPAIR');
 			ELSE
 				DBMS_OUTPUT.put_line('Error: Only one item allowed for Single contracts');
 				Insert into RepairItem values(n_item, model, l_price, l_year, 'NONE', 'COMPUTER');
-				Insert into RepairJob values(n_item, NULL, l_phone, NULL, to_date(SYSDATE, 'DD-MM-YYYY'), 'UNDER_REPAIR');
+				Insert into RepairJob values(n_item, NULL, l_phone, NULL, to_date(SYSDATE, 'DD-MM-YY'), 'UNDER_REPAIR');
 			END IF;
 		END IF;
 	ELSE
 		DBMS_OUTPUT.put_line('Contract not valid');
      		Insert into RepairItem values(n_item, model, l_price, l_year, 'NONE', 'COMPUTER');
-		Insert into RepairJob values(n_item, NULL, l_phone, NULL, to_date(SYSDATE, 'DD-MM-YYYY'), 'UNDER_REPAIR');
+		Insert into RepairJob values(n_item, NULL, l_phone, NULL, to_date(SYSDATE, 'DD-MM-YY'), 'UNDER_REPAIR');
 	END IF;
-
+	
+	Insert into CustomerBill values(l_phone, l_date, NULL, NULL, NULL);
+	commit;
 END;
 /
 Show errors;
@@ -67,15 +72,122 @@ BEGIN
 	Update RepairJob
 	Set status = n_status
 	Where itemId = item;
+
+	commit;
 END;
 /
 Show errors;
 
-Create or Replace Procedure insertProblemCode(item in VARCHAR, code in INTEGER)
+Create or Replace Function getMachineStatus(item in VARCHAR, phone in VARCHAR) return VARCHAR
 AS
 
+l_status RepairJob.status%type;
+
 BEGIN
-	Insert into ProblemReport values(item, code, date);
+	Select status into l_status
+	From RepairJob
+	Where itemId = item and custPhone = phone;
+
+	return l_status;
 END;
 /
-Show errors;	
+Show errors;
+
+Create or Replace Procedure updateProblemDescrip(descrip in VARCHAR, phone in VARCHAR, time in DATE)
+AS
+BEGIN
+	Update RepairLog
+	Set description = descrip
+	Where custPhone = phone and doneDate = time;
+END;
+/
+Show errors;
+
+Create or Replace Function getRevenueGenerated(s_date in DATE, e_date in DATE) return NUMBER
+AS
+Cursor Bill_cur is
+        Select * from CustomerBill;
+Cursor Log_cur is
+        Select * from RepairLog;
+
+l_bill Bill_cur%rowtype;
+l_log Log_cur%rowtype;
+l_revenue NUMBER(6,2) := 0.00;
+
+BEGIN
+        for l_log in Log_cur
+        loop
+                IF l_log.timeOfArrival >= s_date AND l_log.doneDate <= e_date THEN
+                        For l_bill in Bill_cur
+                        loop
+                                IF l_bill.custPhone = l_log.custPhone AND l_bill.timeOfArrival = l_log.timeOfArrival THEN
+                                        l_revenue := l_revenue + l_bill.total;
+                                END IF;
+                        END LOOP;
+                END IF;
+        END LOOP;
+
+	return l_revenue;
+END;
+/
+Show errors;
+
+Create or Replace Function getRevenueNoWarranty(s_date in DATE, e_date in DATE) return NUMBER
+AS
+
+Cursor Bill_cur is
+        Select * from CustomerBill;
+Cursor Log_cur is
+        Select * from RepairLog;
+
+l_bill Bill_cur%rowtype;
+l_log Log_cur%rowtype;
+l_revenue NUMBER(6,2) := 0.00;
+
+BEGIN
+        for l_log in Log_cur
+        loop
+                IF l_log.timeOfArrival >= s_date AND l_log.doneDate <= e_date AND l_log.contractID IS NULL THEN
+                        For l_bill in Bill_cur
+                        loop
+                                IF l_bill.custPhone = l_log.custPhone AND l_bill.timeOfArrival = l_log.timeOfArrival THEN
+                                        l_revenue := l_revenue + l_bill.total;
+                                END IF;
+                        END LOOP;
+                END IF;
+        END LOOP;
+
+	return l_revenue;
+END;
+/
+Show errors;
+
+Create or Replace Function getRevenueWarranty(s_date in DATE, e_date in DATE) return NUMBER
+AS
+Cursor Bill_cur is
+	Select * from CustomerBill;
+Cursor Log_cur is
+	Select * from RepairLog;
+
+l_bill Bill_cur%rowtype;
+l_log Log_cur%rowtype;
+l_revenue NUMBER(6,2) := 0.00;
+
+BEGIN
+	for l_log in Log_cur
+	loop
+		IF l_log.timeOfArrival >= s_date AND l_log.doneDate <= e_date AND l_log.contractID IS NOT NULL THEN
+			For l_bill in Bill_cur
+			loop
+				IF l_bill.custPhone = l_log.custPhone AND l_bill.timeOfArrival = l_log.timeOfArrival THEN
+					l_revenue := l_revenue + 50 + l_bill.costOfParts + (l_bill.laborHours * 20);
+				END IF;
+			END LOOP;
+		END IF;
+	END LOOP;
+
+	return l_revenue;
+END;
+/
+Show errors;
+
--- sql/triggers.sql
+++ sql/triggers.sql
@@ -4,20 +4,23 @@
 
 Create or Replace TRIGGER RepairJob_after_update_trig
 After UPDATE Of status ON RepairJob
+	FOR EACH ROW
 DECLARE
-	v_job RepairJob%rowtype;
+	Cursor Job_cur is Select * from RepairJob;
+	v_job Job_cur%rowtype;
 	v_contractType RepairItem.contractType%type;
 
 BEGIN
 	Select * into v_job
 	From RepairJob
-	Where custPhone = :new.custPhone AND timeOfArrival = :new.timeOfArrival
+	Where custPhone = :new.custPhone AND timeOfArrival = :new.timeOfArrival;
 
 	IF v_job.status = 'DONE' THEN
-		Insert into RepairLog values(v_job.itemId, v_job.contractId, v_job.custPhone,
+		Insert into RepairLog values(v_job.itemId, v_job.contractId, NULL, v_job.custPhone,
 			v_job.custPhone, v_job.empNo, v_job.timeOfArrival,
-			to_date(SYSDATE, 'DD-MM-YYYY'), v_job.status);
-		Delete From RepairJob Where custPhone = v_job.custPhone  AND timeOfArrival = v_job.timeOfArrival AND itemId = v_job.itemId;
+			to_date(SYSDATE, 'DD-MM-YY'), v_job.status);
+
+		Delete From RepairJob Where custPhone = v_job.custPhone AND timeOfArrival = v_job.timeOfArrival AND itemId = v_job.itemId;
 		
 		Select contractType into v_contractType
 		From RepairItem
@@ -30,8 +33,8 @@ BEGIN
 			Where custPhone = v_job.custPhone AND timeOfArrival = v_job.timeOfArrival;
 		ELSE
 			Update CustomerBill
-			Set laborHours = DBMS_RANDOM.value(1, 10), costOfParts = 0,
-				total = 0;
+			Set laborHours = DBMS_RANDOM.value(1, 10), costOfParts = DBMS_RANDOM.value(10.00, 30.00),
+				total = 0
 			Where custPhone = v_job.custPhone AND timeOfArrival = v_job.timeOfArrival;
 		END IF;
 	END IF;
