47cbf46ff4592a5471ba532ce6bae4a4f2185b34
daariff <aariff.deen@gmail.com>
2018-03-12 20:21:31 -0700

    minor update

--- sql/tests.sql
+++ sql/tests.sql
@@ -7,10 +7,7 @@ INSERT INTO RepairItem VALUES ('001','Samsung',40.00,2017,'SINGLE','COMPUTER');
 INSERT INTO ServiceContract VALUES('S01',DATE '2015-12-10',DATE '2015-12-17','408-663-7143','001',NULL,'SINGLE');
 INSERT INTO RepairPerson VALUES('e01','john smith','408-666-6666');
 
-SELECT * FROM RepairLog;
-
-
-INSERT INTO RepairLog VALUES('001','S01','No Problems','408-663-7143','e01',DATE '2015-12-10',DATE '2015-12-17','DONE');
+--INSERT INTO RepairLog VALUES('001','S01','No Problems','408-663-7143','e01',DATE '2015-12-10',DATE '2015-12-17','DONE');
 
 
 /*acceptMachine(n_name in VARCHAR, n_item in VARCHAR, model in VARCHAR, cId in VARCHAR, in_date in DATE, message OUT VARCHAR2)*/
@@ -19,5 +16,7 @@ DECLARE
 BEGIN
    acceptMachine('Deen','002','Samsung','408-663-7143','2015-12-11',msg);
 END;
+
+SELECT * FROM RepairLog;
 
 commit;
