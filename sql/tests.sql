5131a661944288306d7a2cec066ff409b20a40be
daariff <aariff.deen@gmail.com>
2018-03-12 20:28:11 -0700

    minor update

--- sql/tests.sql
+++ sql/tests.sql
@@ -8,12 +8,11 @@ INSERT INTO RepairLog VALUES('001','S01','No Problems','408-663-7143','e01',DATE
 
 
 --acceptMachine(n_name in VARCHAR, n_item in VARCHAR, model in VARCHAR, cId in VARCHAR, in_date in DATE, message OUT VARCHAR2)*/
---DECLARE
---	msg  NVARCHAR2 (20) := '';
---BEGIN
---   acceptMachine('Deen','002','Samsung','408-663-7143','2015-12-11',msg);
---END;
---
---SELECT * FROM RepairLog;*/
+DECLARE
+	msg  NVARCHAR2 (20) := '';
+BEGIN
+   acceptMachine('Deen','002','Samsung','408-663-7143','2015-12-11',msg);
+END;
+
 
 commit;
