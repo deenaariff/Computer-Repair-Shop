824b85f6346353bf875c3d9aa3e2dd9928e56060
daariff <aariff.deen@gmail.com>
2018-03-12 20:18:46 -0700

    minor update

--- sql/tests.sql
+++ sql/tests.sql
@@ -1,5 +1,4 @@
 --- Tests for Customer Bill
-start tables;
 start procedures;
 
 INSERT INTO Customers VALUES ('408-663-7143','Deen Aarif');
@@ -7,7 +6,7 @@ INSERT INTO CustomerBill VALUES ('408-663-7143',DATE '2015-12-17',20.00,40.00,5)
 INSERT INTO RepairItem VALUES ('001','Samsung',40.00,2017,'SINGLE','COMPUTER');
 INSERT INTO ServiceContract VALUES('S01',DATE '2015-12-10',DATE '2015-12-17','408-663-7143','001',NULL,'SINGLE');
 INSERT INTO RepairPerson VALUES('e01','john smith','408-666-6666');
---INSERT INTO RepairLog VALUES('001','S01','No Problems','408-663-7143','e01',DATE '2015-12-10',DATE '2015-12-17','DONE');
+INSERT INTO RepairLog VALUES('001','S01','No Problems','408-663-7143','e01',DATE '2015-12-10',DATE '2015-12-17','DONE');
 
 
 /*acceptMachine(n_name in VARCHAR, n_item in VARCHAR, model in VARCHAR, cId in VARCHAR, in_date in DATE, message OUT VARCHAR2)*/
