24f5dbfc8d6b8253ca34c17d98c010569bfe0de0
daariff <aariff.deen@gmail.com>
2018-03-13 23:54:13 -0700

    minor update

---
+++ revenue/revenue.js
@@ -0,0 +1,52 @@
+(function(scope) {
+
+	console.log('acceptMachine.js has been invoked')
+
+	$(document).ready(function(){
+
+		$('.submit_button').click(function(e) {	
+
+		  e.preventDefault()  
+
+		  // debug
+		  console.log("Accept Machine Submit button was clicked")
+
+		  missing = false;
+		  message = "";
+
+		  model = "";
+
+		  var data = $('.revenueForm').serializeArray().reduce(function(obj, item) {
+		  	if (item.value == "") {
+		  		message += "[FIELD]: " + item.name + " is missing. "
+		  		missing = true;
+		  	} else if (item.name == "model") {
+		  		model = item.value;
+			}
+			obj[item.name] = item.value;
+			return obj;
+		  }, {});
+
+		  if(missing == true) {
+		  	alert(message)
+		  	return;
+		  }
+
+		  var callback = function(result,message) {
+		  	if(result == 0) {
+		  		alert("New Machine '" + model + "' was Accepted");
+		  	} else {
+		  		alert("An Error Occured. " + message)
+		  	}
+		  }
+
+		  console.log(data);
+
+		  // call function from dbTransactions.js
+		  //scope.fetchPHPdata(data, "acceptMachine.php", callback);
+
+		});
+
+	});
+
+})(this);
