167613a4556623e1007a0229dfa4148875b8fc5e
maggie lingqi cai <mcai@linux60806.dc.engr.scu.edu>
2018-03-13 23:11:31 -0700

    minor update

--- sql/tests.sql
+++ sql/tests.sql
@@ -3,7 +3,7 @@ start tables;
 
 INSERT INTO Customers VALUES ('408-663-7143','Deen Aarif');
 INSERT INTO RepairItem VALUES ('001','Samsung',40.00,2017,'SINGLE','COMPUTER');
-INSERT INTO CustomerBill VALUES ('001','408-663-7143',DATE '2015-12-17',20.00,40.00,5);
+INSERT INTO CustomerBill VALUES ('001','408-663-7143',DATE '2015-12-10',20.00,40.00,5);
 INSERT INTO ServiceContract VALUES('S01',DATE '2015-12-10',DATE '2015-12-17','408-663-7143','001',NULL,'SINGLE');
 INSERT INTO RepairPerson VALUES('e01','john smith','408-666-6666');
 INSERT INTO RepairLog VALUES('001','S01','No Problems','408-663-7143','e01',DATE '2015-12-10',DATE '2015-12-17','DONE');
@@ -36,7 +36,7 @@ end;
 declare
 	output number := 0;
 begin
-	output := getRevenueGenerated(DATE '2015-12-8', DATE '2015-12-16');
+	output := getRevenueGenerated(DATE '2015-12-08', DATE '2015-12-17');
 	dbms_output.put_line('getRevenueGenerated() : ' || output);
 end;
 /
@@ -46,7 +46,7 @@ end;
 declare
 	output number := 0;
 begin
-	output := getRevenueNoWarranty(DATE '2015-12-8', DATE '2015-12-16');
+	output := getRevenueNoWarranty(DATE '2015-12-08', DATE '2015-12-17');
 	dbms_output.put_line('getRevenueNoWarranty() : ' || output);
 end;
 /
@@ -56,8 +56,8 @@ end;
 declare
 	output number := 0;
 begin
-	output := getRevenueWarranty(DATE '2015-12-8', DATE '2015-12-16');
-	dbms_output.put_line('getRevenueNoWarranty() : ' || output);
+	output := getRevenueWarranty(DATE '2015-12-08', DATE '2015-12-17');
+	dbms_output.put_line('getRevenueWarranty() : ' || output);
 end;
 /
 
