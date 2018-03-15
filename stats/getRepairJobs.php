436edb4f0f32d3095eb292042a37c6aacb5ac674
daariff <aariff.deen@gmail.com>
2018-03-15 12:32:18 -0700

    minor update

--- stats/getRepairJobs.php
+++ stats/getRepairJobs.php
@@ -9,7 +9,7 @@ if(!$conn) {
 }
 
 
-$queryString = "SELECT itemID, contractID, custPhone, empNo, TIMEOFARR, DONEDATE FROM RepairLogs";
+$queryString = "SELECT itemID, contractID, custPhone, empNo, TIMEOFARR, DONEDATE FROM RepairLog";
 
 $query = oci_parse($conn,$queryString);
 $res = oci_execute($query);
