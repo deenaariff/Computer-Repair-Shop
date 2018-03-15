ab78c5ae1bc3fab8c7873cf45264d935c9159972
daariff <aariff.deen@gmail.com>
2018-03-14 18:43:37 -0700

    minor update

--- getRepairJobs/getRepairJobs.js
+++ getRepairJobs/getRepairJobs.js
@@ -15,18 +15,15 @@
 
 		/* update the model with a given array of data */
 		var updateRepairJobs = function(total_data) {
-			console.log('break2: reached');
-			console.log(total_data);
 			for(var i = 0; i < total_data.length; i ++) {
 				var data = total_data[i].split("|");
+				console.log(data);
 				$('#repairTable').createRow(data[0],data[1],data[2],data[3],data[4]);
 			}
 		};
 
 		/* callback to handle call to PHP file */
 		var callback = function(result,data) {
-			console.log("break1: " + (result == 0));
-			console.log("break2: " + data);
 		  	if(result == 0) {
 		  		updateRepairJobs(data);
 		  	} else {
