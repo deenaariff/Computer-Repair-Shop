e5c55aa254e0af7a9ccecc06ef842d2d6ed174e0
daariff <aariff.deen@gmail.com>
2018-03-14 18:45:45 -0700

    minor update

--- getRepairJobs/getRepairJobs.js
+++ getRepairJobs/getRepairJobs.js
@@ -2,7 +2,7 @@
 
 	/* Helper function to generate html for a table row of data*/
 	var createRow = function (itemid,custphone,empno,timeofarrival,status) {
-		return "<tr class='row_entry "+classr+"'>" +
+		return "<tr class='row_entry'>" +
                "<td>" + itemid + "</td>" +
                "<td>" + custphone + "</td>" +
                "<td>" + empno + "</td>" +
@@ -17,7 +17,9 @@
 		var updateRepairJobs = function(total_data) {
 			for(var i = 0; i < total_data.length; i ++) {
 				var data = total_data[i].split("|");
-				$('#repairTable').append(createRow(data[0],data[1],data[2],data[3],data[4]));
+				var html = createRow(data[0],data[1],data[2],data[3],data[4]);
+				console.log(html);
+				$('#repairTable').append(html);
 			}
 		};
 
