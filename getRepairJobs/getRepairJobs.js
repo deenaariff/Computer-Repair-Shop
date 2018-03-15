3e4d5f16ea1af644da86036cd1580542262407d7
daariff <aariff.deen@gmail.com>
2018-03-14 22:08:00 -0700

    minor update

--- getRepairJobs/getRepairJobs.js
+++ getRepairJobs/getRepairJobs.js
@@ -15,6 +15,7 @@
 
 		/* update the model with a given array of data */
 		var updateRepairJobs = function(total_data) {
+			console.log(total_data);
 			for(var i = 0; i < total_data.length; i ++) {
 				var data = total_data[i].split("|");
 				var html = createRow(data[0],data[1],data[2],data[3],data[4]);
